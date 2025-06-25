25fea0a0c5521eae530572dc08d00278d149373d
Lucas0101Ly <132003590+Lucas0101Ly@users.noreply.github.com>
2025-06-25 08:14:53 -0300

    t1

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você prefere ter vários amores na vida, ou se relacionar com a mesma mulher para sempre",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "ter vários amores",
+                afirmacao: "Você é uma pessoa de personalidade livre e quer viver várias aventuras "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "UM único amor",
+                afirmacao: "Você não ve graça em viver apenas em prol dos desejos carnais e deseja algo duradouro"
             }
         ]
     },
